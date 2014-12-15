dcaae488e02a45250f1621e2656cde486d71bb10
Sophia Shapira <Sophia.Shapira@gmail.com>
2014-12-14 23:17:35 -0500

    Fixed a problem

--- plmod/chorebox/raywork.pm
+++ plmod/chorebox/raywork.pm
@@ -2,15 +2,13 @@
 package chorebox::raywork;
 use strict;
 
-our @EXPORT = qw(chore_good_array,chore_bad_array);
-
-sub chore_good_array {
+sub good {
   my $lc_cnt;
   $lc_cnt = @_;
   return ( $lc_cnt > 0.5 );
 }
 
-sub chore_bad_array {
+sub bad {
   my $lc_cnt;
   $lc_cnt = @_;
   return ( $lc_cnt < 0.5 );
