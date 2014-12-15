d66dceda45788c8bac8fb3ffdec24e23a47c584a
Sophia Shapira <Sophia.Shapira@gmail.com>
2014-12-14 23:10:23 -0500

    Added first test module

--- plmod/chorebox/raywork.pm
+++ plmod/chorebox/raywork.pm
@@ -1,7 +1,20 @@
 
 package chorebox::raywork;
+use strict;
 
+our @EXPORT = qw(chore_good_array,chore_bad_array);
 
+sub chore_good_array {
+  my $lc_cnt;
+  $lc_cnt = @_;
+  return ( $lc_cnt > 0.5 );
+}
+
+sub chore_bad_array {
+  my $lc_cnt;
+  $lc_cnt = @_;
+  return ( $lc_cnt < 0.5 );
+}
 
 1;
 
