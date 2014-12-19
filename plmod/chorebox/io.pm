45700951a264f094abec5cb251551895fcc21b50
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2014-12-19 17:53:36 -0500

    Fixed a bug.

--- plmod/chorebox/io.pm
+++ plmod/chorebox/io.pm
@@ -1,8 +1,13 @@
 package chorebox::io;
 use strict;
 
+my @infiles = ("/dev/stdin","/proc/self/fd/0");
 my @outfiles = ("/dev/stdout","/proc/self/fd/1");
 my @errfiles = ("/dev/stderr","/proc/self/fd/2");
+
+sub ins {
+  return &firstof(@infiles);
+}
 
 sub outs {
   return &firstof(@outfiles);
@@ -16,7 +21,7 @@ sub firstof {
   my $lc_a;
   foreach $lc_a (@_)
   {
-    if ( -f $lc_a ) { return $lc_a; }
+    if ( -e $lc_a ) { return $lc_a; }
   }
   return ( 1 > 2 );
 }
